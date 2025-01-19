1233902112bfd02d0708537afe5679f0ce32e577
MBlich <116032823+MBlich@users.noreply.github.com>
2025-01-19 21:56:29 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
+import Header from "@/components/header";
+import Hero from "@/components/hero";
+import About from "@/components/about";
+import Offer from "@/components/offer";
+import Footer from "@/components/footer";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <Hero />
+      <div id="o-mnie">
+        <About />
+      </div>
+      <div id="oferta">
+        <Offer />
+      </div>
+      <div id="kontakt">
+        <Footer />
+      </div>
+    </main>
+  );
+}
+
+
+
 import { Metadata } from 'next'
 import Header from '@/components/header'
 import Hero from '@/components/hero'
