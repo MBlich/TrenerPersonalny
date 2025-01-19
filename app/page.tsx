4a3cc12a99b9db86e577026e0bf7e44e2af10111
MBlich <116032823+MBlich@users.noreply.github.com>
2025-01-19 21:57:43 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,45 +24,45 @@ export default function Home() {
 
 
 
-import { Metadata } from 'next'
-import Header from '@/components/header'
-import Hero from '@/components/hero'
-import About from '@/components/about'
-import Offer from '@/components/offer'
-import Footer from '@/components/footer'
+// import { Metadata } from 'next'
+// import Header from '@/components/header'
+// import Hero from '@/components/hero'
+// import About from '@/components/about'
+// import Offer from '@/components/offer'
+// import Footer from '@/components/footer'
 
-export const metadata: Metadata = {
-  title: 'Strona główna | Trener Marcin Blicharski',
-  description: 'Profesjonalne treningi personalne z boksu, kickboxingu i kondycyjne we Wrocławiu. Skorzystaj z doświadczenia Mistrza Polski i medalisty Mistrzostw Świata.',
-}
+// export const metadata: Metadata = {
+//   title: 'Strona główna | Trener Marcin Blicharski',
+//   description: 'Profesjonalne treningi personalne z boksu, kickboxingu i kondycyjne we Wrocławiu. Skorzystaj z doświadczenia Mistrza Polski i medalisty Mistrzostw Świata.',
+// }
 
-export default function Home() {
-  return (
-    <main>
-      <Header />
-      <Hero />
-      <About />
-      <Offer />
-      <Footer />
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "@context": "http://schema.org",
-            "@type": "Person",
-            "name": "Marcin Blicharski",
-            "jobTitle": "Trener personalny oraz instruktor Boksu",
-            "description": "Wieloletni członek Kadry Narodowej Taekwon-do i Kickboxingu",
-            "telephone": "+48796916466",
-            "url": "https://www.example.com",
-            "sameAs": [
-              "https://www.facebook.com/example",
-              "https://www.instagram.com/example"
-            ]
-          })
-        }}
-      />
-    </main>
-  )
-}
+// export default function Home() {
+//   return (
+//     <main>
+//       <Header />
+//       <Hero />
+//       <About />
+//       <Offer />
+//       <Footer />
+//       <script
+//         type="application/ld+json"
+//         dangerouslySetInnerHTML={{
+//           __html: JSON.stringify({
+//             "@context": "http://schema.org",
+//             "@type": "Person",
+//             "name": "Marcin Blicharski",
+//             "jobTitle": "Trener personalny oraz instruktor Boksu",
+//             "description": "Wieloletni członek Kadry Narodowej Taekwon-do i Kickboxingu",
+//             "telephone": "+48796916466",
+//             "url": "https://www.example.com",
+//             "sameAs": [
+//               "https://www.facebook.com/example",
+//               "https://www.instagram.com/example"
+//             ]
+//           })
+//         }}
+//       />
+//     </main>
+//   )
+// }
 
