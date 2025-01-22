72986e83a6d113d6c09c353403ad7205c74cebab
MBlich <116032823+MBlich@users.noreply.github.com>
2025-01-22 23:17:20 +0100

    Create metadata.ts

---
+++ app/metadata.ts
@@ -0,0 +1,19 @@
+import type { Metadata } from "next"
+
+export const defaultMetadata: Metadata = {
+  title: "Trener Personalny Marcin Blicharski| Profesjonalny trening personalny",
+  description:
+    "Profesjonalny trener personalny. Indywidualne plany treningowe, konsultacje online, treningi personalne i więcej.",
+  keywords: ["trener personalny", "trening personalny", "indywidualny plan treningowy", "trening online"],
+  robots: "index, follow",
+  openGraph: {
+    type: "website",
+    locale: "pl_PL",
+    url: "https:/trenerpersonalny-blicharski.pl",
+    title: "Trener Personalny | Profesjonalny trening personalny",
+    description:
+      "Profesjonalny trener personalny. Indywidualne plany treningowe, konsultacje online, treningi personalne i więcej.",
+    siteName: "Trener Personalny",
+  },
+}
+
